371b5a06dc31d7cc1813e52bf34378bfafd2dc18
Morten Høgh <mortenhogh@gmail.com>
2024-07-08 11:17:20 +0200

    Fixed warning. Again

--- lua/test-buffer.lua
+++ lua/test-buffer.lua
@@ -3,19 +3,19 @@ local function set_plugin(options)
   return print(options.name)
 end
 local function setup_autocmd(options)
-  local augroup = vim.api.nvim_create_augroup("TestBuffer", {clear = true})
+  local augroup = _G.vim.api.nvim_create_augroup("TestBuffer", {clear = true})
   local function _1_()
     return set_plugin(options)
   end
-  return vim.api.nvim_create_autocmd("VimEnter", {group = augroup, desc = "Set a fennel scratch buffer on load", once = true, callback = _1_})
+  return _G.vim.api.nvim_create_autocmd("VimEnter", {group = augroup, desc = "Set a fennel scratch buffer on load", once = true, callback = _1_})
 end
 local function setup(user_options)
-  if not (vim.fn.argc() == 0) then
+  if not (_G.vim.fn.argc() == 0) then
     return
   else
   end
   local default_options = {filetype = "fennel", name = "default-name"}
-  local options = vim.tbl_extend("force", default_options, (user_options or {}))
+  local options = _G.vim.tbl_extend("force", default_options, (user_options or {}))
   if (maps[options.filetype] == nil) then
     error(("Filetype " .. options.filetype .. " is not supported"))
   else
